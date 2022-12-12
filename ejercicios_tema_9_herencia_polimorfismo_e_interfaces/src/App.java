71d89a3ccb4699bd88897b966d6ad1acd9636d68
sebasMV W7 <stimotev11@gmail.com>
2022-12-12 00:26:44 -0500

    superejemplo abstraccion

--- ejercicios_tema_9_herencia_polimorfismo_e_interfaces/src/App.java
+++ ejercicios_tema_9_herencia_polimorfismo_e_interfaces/src/App.java
@@ -1,35 +1,57 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Coche myCoche = new Coche();
-        myCoche.velocidadMaxima = 14;/*aunque la clase coche este vacia hereda de su padre vehiculo sus funciones y propiedades */
+        myCoche.setSonido("BRRRerere");
+        System.out.println(myCoche.getsonido());
+
+        Moto myMoto = new Moto();
+        myMoto.setSonido("rannnnn");
+        System.out.println(myMoto.getsonido());
+        //una clase abstracta no se puede instanciar directamente se debe hacer una clase que la herede
+        // Vehiculo myVehiculo = new Vehiculo();
+        // myVehiculo.setSonido("BRRRrrrr");
+        // System.out.println(myVehiculo.getsonido());
         
-        CocheElectrico myCocheE = new CocheElectrico();
-        myCocheE.matricula = "EVA46F";
-        System.out.println(myCocheE.compruebaMatricula("abc"));
     }
 }
-class Vehiculo {
+abstract class Vehiculo {
     int velocidadMaxima;
     String matricula;
-    String tipoGasolina;
+    String sonido;
 
-    public boolean compruebaMatricula(String matricula) {
-        if (matricula == "XXX") {
-            return true;
-        }
-        return false;
+    public Vehiculo(){
+        System.out.println("estoy en el constructor de vehiculo");
     }
+    //las funciones abstractas no tiene cuerpo
+    abstract public String getsonido();
+    abstract public void setSonido(String sonido);
 }
 /*ejemplo de herencia clase hija */
-// final class Coche extends Vehiculo {  
-// }
+
 class Coche extends Vehiculo {
+//como vehiculo es abstracto tengo que implementar en Coche los metodos abstractos en Vehiculo(getSonido y setSonido) y darle una funcionalidad
+// ya que si llegara a necesitar un metodo de Vehiculo y estos metodos son adstractos sin cuerpo, no sabria que hacen
+// por eso en Coche debo implementarlos y darle una funcionalidad si no sale un error
+
+public String getsonido(){
+    return "Soy un super sonido " + this.sonido;// retorno el valor de la variable de la clase padre es decir Vehiculo
+}
+public void setSonido(String sonido){
+    this.sonido = sonido;
+}
+
 
   
 }
 
 
-class CocheElectrico extends Coche {
+class Moto extends Vehiculo {
 
+    public String getsonido(){
+        return "Soy un super sonidito de moto " + this.sonido;// retorno el valor de la variable de la clase padre es decir Vehiculo
+    }
+    public void setSonido(String sonido){
+        this.sonido = sonido;
+    }
     
 }
