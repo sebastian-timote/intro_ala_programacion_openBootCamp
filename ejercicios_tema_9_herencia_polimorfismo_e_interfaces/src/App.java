f623e36f2af921b10a7f30e8a4eac215485a9c55
sebasMV W7 <stimotev11@gmail.com>
2022-12-29 00:02:56 -0500

    ejemplo de interface

--- ejercicios_tema_9_herencia_polimorfismo_e_interfaces/src/App.java
+++ ejercicios_tema_9_herencia_polimorfismo_e_interfaces/src/App.java
@@ -1,40 +1,27 @@
 public class App {
-    //POLIMORFISMO - CUANDO UNA CLASE HIJA IMPLEMENTA UNA FUNCION DEL PADRE PERO HACE ALGO DISTINTO
-    //otro ejemplo polimorfismo podemos ver la funcion sumaNumeros que es polimorfica ya que se llama igual
-    //pero se diferencia del dato que se le pase, si se lepasan datos incorrectos arrojara error
+    //INTERFACES
+    //utilizamos las interfaces cuando queremos crear clases y esas clases con unas funciones ya definidas
+    //los programadores se ponen deacuerdo para que las clases queden con las mismas funciones 
+    //las interfaces se utilizan para unificar metodos
+    //las ventajas de las interfaces es que si yo se que una clase la implementa tengo la certeza de los
+    //metodos que va a tener
     public static void main(String[] args) throws Exception {
         
-        Coche myCoche = new Coche();
-        //System.out.println(myCoche.sumaNumeros(8, 1));
-        //System.out.println(myCoche.sumaNumeros((float)8, (float)1));
-        myCoche.sumaNumeros(2.3, 5.2);
-
-
-        
     }
 }
-class Vehiculo {
-    
-    public void diHola() {
-        System.out.println("HOLA!!");
-    }
+interface Vehiculo {
+//yo no implemento la funcion pero si le digo las funciones que debe implementar el programador
+    void Acelerar(int cuantaVelocidad);
+    void Frenar(int cuantaVelocidad);
 }
-class Coche extends Vehiculo{
-    public void diHola() {
-       System.out.println("soy un coche!!");
-    }
 
-    public int sumaNumeros(int a, int b) {
-        System.out.println("soy el sumanumeros de enteros");
-        return a + b;
+class Coche implements Vehiculo{
+//no voy a heredar la interface la voya implementar
+    public void Acelerar(int cuantaVelocidad){
+
     }
-    public Float sumaNumeros(Float a, Float b) {
-        System.out.println("soy el sumanumeros de float");
-        return a + b * (float)9.2;
-    }
-    public void sumaNumeros(Double a, Double b) {
-        System.out.println("soy el sumanumeros de double");
-        System.out.println("el resultado es: " + (a + b));
+    public void Frenar(int cuantaVelocidad){
+
     }
 }
 
