4bfb13fe5515a0639a1f73aed8aef06710f90a50
sebasMV W7 <stimotev11@gmail.com>
2022-12-30 20:27:21 -0500

    ejemplo 1 metodos de clases

---
+++ ejercicios_tema_10_metodos_de_clase/tema10MetodosDeClase/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        var resultado = suma(5, 2);
+        System.out.println(resultado);
+
+    }
+    public static int suma(int operA, int operB) {
+        return operA + operB;
+    }
+}
