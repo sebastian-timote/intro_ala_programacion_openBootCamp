14bdd2e73c0b5fdb87fd21b2e991787e1d9ef8b5
sebasMV W7 <stimotev11@gmail.com>
2022-12-30 22:08:50 -0500

    ejemplo interface y metodo graper

--- ejercicios_tema_10_metodos_de_clase/tema10MetodosDeClase/src/App.java
+++ ejercicios_tema_10_metodos_de_clase/tema10MetodosDeClase/src/App.java
@@ -1,10 +1,42 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        var resultado = suma(5, 2);
-        System.out.println(resultado);
+        Coche myCoche = new Coche();
+        Moto myMoto = new Moto();
+        ejecutaAcelerar(myMoto);
+        ejecutaAcelerar(myCoche);
+
+
 
     }
-    public static int suma(int operA, int operB) {
-        return operA + operB;
+    //el parametro que le pase tiene q implementar la interface Vehiculo
+    //es llamada graper un funcion que se ejecuta si se le pasa un objeto que satisfaga la interface Vehiculo
+    public static void ejecutaAcelerar(Vehiculo vehiculo) {
+        vehiculo.acelerar(15);
+    }
+}
+
+interface Vehiculo {
+    void acelerar(int cuantaVelocidad);
+    void frenar(int cuantaVelocidad);
+    
+}
+
+class Coche implements Vehiculo{
+
+    public void acelerar(int cuantaVelocidad){
+        System.out.println("coche() - acelerar()");
+    }
+    public void frenar(int cuantaVelocidad){
+        System.out.println("coche() - frenar()");
+    }
+}
+
+class Moto implements Vehiculo{
+
+    public void acelerar(int cuantaVelocidad){
+        System.out.println("Moto() - acelerar()");
+    }
+    public void frenar(int cuantaVelocidad){
+        System.out.println("Moto() - frenar()");
     }
 }
